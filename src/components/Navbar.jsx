d720ffa81b3622cc1b5ed0794d7e678d195f3545
unknown <opakunleeniolail2019@gmail.com>
2024-07-10 19:02:25 +0100

    feat: changed some p tags to buttons and a href

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,46 @@
 import React from "react";
 import logo from "../assets/logo.svg";
-import cartImg from '../assets/icon-cart.svg'
-import avatar from '../assets/image-avatar.png'
+import cartImg from "../assets/icon-cart.svg";
+import avatar from "../assets/image-avatar.png";
 
 function Navbar() {
   return (
     <div className="fixed top-0 px-14">
-      <div className="  flex py-5 justify-between items-center w-[1400px]  m-auto border-b-2">
-        <div className="flex gap-x-12" >
+      <div className="  flex px-10 py-5 justify-between items-center w-[1400px]  m-auto border-b-2">
+        <div className="flex gap-x-12">
           <a href="">
             <img src={logo} alt="sneakers" />
           </a>
           <div className="flex gap-x-6">
-            <p className="text-red">collections</p>
-            <p className="transition ease-in-out hover:text-red-500 duration-300">Men</p>
-            <p className="hover:text-red-500">Women</p>
-            <p className="hover:text-red-500">About</p>
-            <p className="hover:text-red-500">Contact</p>
+            <a href="#" className="text-black hover:text-orange-500">
+              Collection
+            </a>
+            <a href="#" className="text-black hover:text-orange-500">
+              Men
+            </a>
+            <a href="#" className="text-black hover:text-orange-500">
+              Women
+            </a>
+            <a href="#" className="text-black hover:text-orange-500">
+              About
+            </a>
+            <a href="#" className="text-black hover:text-orange-500">
+              Contact
+            </a>
+    
           </div>
         </div>
         <div className="flex items-center gap-x-6">
-          <img src={cartImg} alt="" className="w-[25px] h-[25px]" />
-          <img src={avatar} alt="" className="w-[50px] border-2 rounded-full border-orange-500"/>
+          <button className=" bg-white hover:border-white">
+            <img src={cartImg} alt="" className="w-[25px] h-[25px]" />
+          </button>
+          <button className=" bg-white hover:border-white">
+            <img
+              src={avatar}
+              alt=""
+              className="w-[50px] border-2 rounded-full border-orange-500"
+            />
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Display.jsx
+++ src/pages/Display.jsx
@@ -3,24 +3,24 @@ import productImg1 from "../assets/image-product-1.jpg";
 import productImg2 from "../assets/image-product-2.jpg";
 import productImg3 from "../assets/image-product-3.jpg";
 import productImg4 from "../assets/image-product-4.jpg";
-import cartImg from '../assets/icon-cart.svg';
-import minusImg from '../assets/icon-minus.svg'
-import plusImg from '../assets/icon-plus.svg'
+import cartImg from "../assets/icon-cart.svg";
+import minusImg from "../assets/icon-minus.svg";
+import plusImg from "../assets/icon-plus.svg";
 
 function Display() {
   const [imgName, setImgName] = useState(productImg1);
-  const [number, setNumber] = useState(0)
+  const [number, setNumber] = useState(0);
   const handleImgChange = (image) => {
     setImgName(image);
   };
-  const handleIncreaseNum = () =>{
-    let num = number + 1
-    setNumber(num)
-  }
-  const handleDecreaseNum = () =>{
-    let num = number -1
-    setNumber(num)
-  }
+  const handleIncreaseNum = () => {
+    let num = number + 1;
+    setNumber(num);
+  };
+  const handleDecreaseNum = () => {
+    let num = number - 1;
+    setNumber(num);
+  };
   return (
     <div className="w-[1400px] px-14">
       <div className="flex mt-[100px] px-10 justify-between">
@@ -70,12 +70,22 @@ function Display() {
           <p className="line-through">$250.00</p>
           <div className="flex gap-x-3">
             <div className="flex items-center gap-x-4 px-7 bg-gray-200 rounded-lg">
-              <p onClick={() => handleDecreaseNum()}><img src={minusImg} alt="" /></p>
+              <button
+                onClick={() => handleDecreaseNum()}
+                className="bg-gray-200 hover:border-gray-200"
+              >
+                <img src={minusImg} alt="" />
+              </button>
               <p>{number}</p>
-              <p onClick={() => handleIncreaseNum()}><img src={plusImg} alt="" /></p>
+              <button
+                onClick={() => handleIncreaseNum()}
+                className="bg-gray-200 hover:border-gray-200"
+              >
+                <img src={plusImg} alt="" />
+              </button>
             </div>
             <div>
-              <button className="bg-orange-500 px-7 flex gap-x-2 items-center">
+              <button className="bg-orange-500 px-9 flex gap-x-2 items-center">
                 <img src={cartImg} alt="" className="" />
                 <span>Add to cart</span>
               </button>
