d35ea888eae54604f40e1048289c03cbb34bcb80
unknown <opakunleeniolail2019@gmail.com>
2024-07-10 13:46:12 +0100

    feat: worked on w

--- src/pages/Display.jsx
+++ src/pages/Display.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import productImg1 from "../assets/image-product-1.jpg";
+import productImg2 from "../assets/image-product-2.jpg";
+import productImg3 from "../assets/image-product-3.jpg";
+import productImg4 from "../assets/image-product-4.jpg";
+import productImg1Th from "../assets/image-product-1-thumbnail.jpg";
 
 function Display() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [imgName, setImgName] = useState(productImg1)
+  const handleImgChange = (image) =>{
+    setImgName(image)
+
+  }
+  return (
+    <div className="w-[1400px] px-14">
+      <div className="flex mt-[100px] px-8">
+        <div className="flex flex-col gap-y-5">
+          <img
+            src={imgName}
+            alt=""
+            className=" h-[350px] rounded-lg"
+          />
+          <div className="flex gap-x-2">
+            <img src={productImg1} alt="" className="w-[120px] rounded-lg" onClick={() => handleImgChange(productImg1)}/>
+            <img src={productImg2} alt="" className="w-[120px] rounded-lg" onClick={() => handleImgChange(productImg2)}/>
+            <img src={productImg3} alt="" className="w-[120px] rounded-lg" onClick={() => handleImgChange(productImg3)}/>
+            <img src={productImg4} alt="" className="w-[120px] rounded-lg" onClick={() => handleImgChange(productImg4)}/>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Display;
